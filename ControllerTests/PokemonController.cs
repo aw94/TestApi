4cf6da6886e80c76682ed9e0081952791b8c31c0
aw94 <awat94@hotmail.com>
2020-02-22 06:09:45 +0100

    Test project added

---
+++ ControllerTests/PokemonController.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+namespace TestApi.ControllerTests
+// No tests implemented yet
+{
+    public class PokemonController
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(0, 0);
+        }
+
+    }
+}
--- Jenkinsfile
+++ Jenkinsfile
@@ -7,7 +7,7 @@ pipeline {
     stages {
         stage('Test') {
             steps {
-                sh 'ls -al'
+                sh 'dotnet test'
             }
         }
     }
